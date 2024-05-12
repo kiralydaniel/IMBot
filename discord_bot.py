f0422455dc9263eeb17b1f13b3effcfb25795df3
kiralydaniel <kiralydani93@gmail.com>
2024-05-12 13:58:22 +0200

    Create discord_bot.py

---
+++ discord_bot.py
@@ -0,0 +1,26 @@
+import os
+
+import utility
+
+
+# Discord bot setup with intents
+bot = utility.bot
+
+spreadsheet = utility.spreadsheet
+
+# Get Sheet1
+runs = utility.sheet1
+
+# Get Sheet2
+balance = utility.sheet2
+
+
+# Fetch the bot token from the environment variable
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+if TOKEN is None:
+    print("Error: Discord bot token not found. Make sure to set the 'DISCORD_BOT_TOKEN' environment variable.")
+    exit(1)
+
+# Run the bot
+bot.run(TOKEN)
